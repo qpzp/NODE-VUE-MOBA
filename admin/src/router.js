c8bb3b3ea58c6dce20fbd7c6aa1df15375a694f7
qpzp <qpzp@hotmail.com>
2019-10-18 11:34:19 +0800

    创建å分类列表

--- admin/src/router.js
+++ admin/src/router.js
@@ -3,7 +3,7 @@ import Router from 'vue-router';
 import Home from './views/Home.vue';
 import Main from './views/Main.vue';
 import CategoryEdit from './views/CategoryEdit.vue';
-// import CategoryList from './views/CategoryList.vue';
+import CategoryList from './views/CategoryList.vue';
 
 Vue.use(Router);
 
@@ -15,7 +15,7 @@ export default new Router({
       component: Main,
       children: [
         {path: '/categories/create', component: CategoryEdit},
-        // {path: '/categories/list', component: CategoryList},
+        {path: '/categories/list', component: CategoryList},
       ]
     },
   ]
--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -3,10 +3,16 @@ module.exports = (app) => {
   const router = express.Router();
   const Category = require('../../models/Categories');
 
+  //创建分类
   router.post('/categories', async (req, res) => {
     const model = await Category.create(req.body);
     res.send(model);
+  });
 
+  //获取分类列表
+  router.get('/categories', async (req, res) => {
+    const items = await Category.find().limit(10);
+    res.send(items);
   });
 
   app.use('/admin/api', router);
