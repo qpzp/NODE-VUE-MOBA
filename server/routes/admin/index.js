68527613d6d168c03d67cf5d23b11b884f3f579a
qpzp <qpzp@hotmail.com>
2019-10-18 16:40:41 +0800

    子分类

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -25,7 +25,7 @@ module.exports = (app) => {
 
   //获取分类列表
   router.get('/categories', async (req, res) => {
-    const items = await Category.find().limit(10);
+    const items = await Category.find().populate('parent').limit(10);
     res.send(items);
   });
 
