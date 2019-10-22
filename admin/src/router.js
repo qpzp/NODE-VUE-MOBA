39d87bbeda557aa9f3f43fa38f1344eb042b4c9b
qpzp <qpzp@hotmail.com>
2019-10-22 15:01:20 +0800

    装备(物品)管理

--- admin/src/router.js
+++ admin/src/router.js
@@ -4,6 +4,8 @@ import Home from './views/Home.vue';
 import Main from './views/Main.vue';
 import CategoryEdit from './views/CategoryEdit.vue';
 import CategoryList from './views/CategoryList.vue';
+import ItemEdit from './views/ItemEdit.vue';
+import ItemList from './views/ItemList.vue';
 
 Vue.use(Router);
 
@@ -14,9 +16,15 @@ export default new Router({
       name: 'main',
       component: Main,
       children: [
+        //分类
         {path: '/categories/create', component: CategoryEdit},
         {path: '/categories/edit/:id', component: CategoryEdit, props: true},
         {path: '/categories/list', component: CategoryList},
+
+        //物品
+        {path: '/items/create', component: ItemEdit},
+        {path: '/items/edit/:id', component: ItemEdit, props: true},
+        {path: '/items/list', component: ItemList},
       ]
     },
   ]
