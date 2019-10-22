88608d6e7b598d340cd039658b41553d25d45488
qpzp <qpzp@hotmail.com>
2019-10-22 16:47:10 +0800

    英雄管理

--- admin/src/router.js
+++ admin/src/router.js
@@ -2,10 +2,18 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 import Main from './views/Main.vue';
+
+//分类
 import CategoryEdit from './views/CategoryEdit.vue';
 import CategoryList from './views/CategoryList.vue';
+
+//物品
 import ItemEdit from './views/ItemEdit.vue';
 import ItemList from './views/ItemList.vue';
+
+//英雄
+import HeroEdit from './views/HeroEdit.vue';
+import HeroList from './views/HeroList.vue';
 
 Vue.use(Router);
 
@@ -25,6 +33,11 @@ export default new Router({
         {path: '/items/create', component: ItemEdit},
         {path: '/items/edit/:id', component: ItemEdit, props: true},
         {path: '/items/list', component: ItemList},
+
+        //英雄
+        {path: '/heroes/create', component: HeroEdit},
+        {path: '/heroes/edit/:id', component: HeroEdit, props: true},
+        {path: '/heroes/list', component: HeroList},
       ]
     },
   ]
