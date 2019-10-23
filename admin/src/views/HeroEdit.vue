3878d936d205b1573615f3a87036540c31addc63
qpzp <qpzp@hotmail.com>
2019-10-23 22:28:53 +0800

    技能编辑【交互】

--- admin/src/views/HeroEdit.vue
+++ admin/src/views/HeroEdit.vue
@@ -82,8 +82,8 @@
                   class="avatar-uploader"
                   :action="$http.defaults.baseURL+'/upload'"
                   :show-file-list="false"
-                  :on-success="afterUpload">
-                  <img v-if="item.avatar" :src="item.avatar" class="avatar">
+                  :on-success="res=>$set(item,'icon',res.url)">
+                  <img v-if="item.icon" :src="item.icon" class="icon">
                   <i v-else class="el-icon-plus avatar-uploader-icon"></i>
                 </el-upload>
               </el-form-item>
@@ -92,6 +92,9 @@
               </el-form-item>
               <el-form-item label="小提示">
                 <el-input v-model="item.tips" type="textarea"></el-input>
+              </el-form-item>
+              <el-form-item>
+                <el-button size="small" type="danger" @click="model.skills.splice(index,1)">删除</el-button>
               </el-form-item>
             </el-col>
           </el-row>
@@ -181,15 +184,15 @@
   .avatar-uploader-icon {
     font-size: 28px;
     color: #8c939d;
-    width: 178px;
-    height: 178px;
-    line-height: 178px;
+    width: 5rem;
+    height: 5rem;
+    line-height: 5rem;
     text-align: center;
   }
 
   .avatar {
-    width: 178px;
-    height: 178px;
+    width: 5rem;
+    height: 5rem;
     display: block;
   }
 </style>
