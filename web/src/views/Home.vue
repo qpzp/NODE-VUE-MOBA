053c77a9a9fe226e295ae73602a697bb3196b036
qpzp <qpzp@hotmail.com>
2019-11-01 20:22:25 +0800

    首页顶部轮播图片 (vue swiper)

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,17 +1,51 @@
 <template>
   <div>
-    主页
+    <swiper :options="swiperOption">
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/85d0686b02abd3488ba227bc82fded66.jpeg" alt="">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/34e5d65cf27b54cb62bfb746df2045b0.jpeg" alt="">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/9d03cadf64aa654eecb27b068b0104f5.jpeg" alt="">
+      </swiper-slide>
+      <div
+        class="swiper-pagination pagination-home text-right px-3 pb-2"
+        slot="pagination"
+      ></div>
+    </swiper>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    data() {
+      return {
+        swiperOption: {
+          pagination: {
+            el: '.pagination-home'
+          }
+        },
+      };
+    }
+  };
 </script>
+
+<style lang="scss">
+  @import "../assets/scss/variables";
+
+  .pagination-home {
+    .swiper-pagination-bullet {
+      opacity: 1;
+      border-radius: 0.1538rem;
+      background: map_get($colors, 'white');
+
+      &.swiper-pagination-bullet-active {
+        background: map_get($colors, 'info');
+      }
+    }
+
+  }
+</style>
