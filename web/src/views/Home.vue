ead5e9147227a0f95d5b8579517f787fea7e00d5
qpzp <qpzp@hotmail.com>
2019-11-01 21:03:25 +0800

    使用精灵图片 (sprite)

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -15,6 +15,19 @@
         slot="pagination"
       ></div>
     </swiper>
+    <!--    end of swiper-->
+    <div class="nav-icons text-center bg-white mt-3 pt-3 text-dark-1">
+      <div class="d-flex flex-wrap">
+        <div class="nav-item mb-3" v-for="n in 10" :key="n">
+          <i class="sprite sprite-news"></i>
+          <div class="py-2">爆料站</div>
+        </div>
+      </div>
+      <div class="bg-light py-2 fs-sm">
+        <i class="sprite sprite-arrow mr-1"></i>
+        <span>收起</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -46,6 +59,18 @@
         background: map_get($colors, 'info');
       }
     }
+  }
 
+  .nav-icons {
+    border-top: 1px solid $border-color;
+    border-bottom: 1px solid $border-color;
+    .nav-item {
+      width: 25%;
+      border-right: 1px solid $border-color;
+
+      &:nth-child(4n) {
+        border-right: none;
+      }
+    }
   }
 </style>
