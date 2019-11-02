e9904108d44cb0ab9454b86239247e50ea14b5c0
qpzp <qpzp@hotmail.com>
2019-11-02 12:41:32 +0800

    补上新闻资讯缺失的nav

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -31,16 +31,35 @@
     <!--    end of nav-icons-->
 
     <m-card icon="menu" title="新闻资讯">
-      <swiper>
-        <swiper-slide v-for="m in 5" :key="m">
-          <div class="py-2" v-for="n in 5" :key="n">
-            <span>[新闻]</span>
-            <span>|</span>
-            <span>11月1日“演员”惩罚名单</span>
-            <span>11/01</span>
-          </div>
-        </swiper-slide>
-      </swiper>
+      <div class="nav jc-between">
+        <div class="nav-item active">
+          <div class="nav-link">热门</div>
+        </div>
+        <div class="nav-item">
+          <div class="nav-link">新闻</div>
+        </div>
+        <div class="nav-item">
+          <div class="nav-link">公告</div>
+        </div>
+        <div class="nav-item">
+          <div class="nav-link">活动</div>
+        </div>
+        <div class="nav-item">
+          <div class="nav-link">赛事</div>
+        </div>
+      </div>
+      <div class="pt-3">
+        <swiper>
+          <swiper-slide v-for="m in 5" :key="m">
+            <div class="py-2" v-for="n in 5" :key="n">
+              <span>[新闻]</span>
+              <span>|</span>
+              <span>11月1日“演员”惩罚名单</span>
+              <span>11/01</span>
+            </div>
+          </swiper-slide>
+        </swiper>
+      </div>
     </m-card>
 
     <m-card icon="helmet-battle" title="英雄列表"></m-card>
